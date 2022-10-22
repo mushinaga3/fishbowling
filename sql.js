cef538814f04c600a7e838bcda8dfa90cb27f9b3
mushi37 <53003074+mushi37@users.noreply.github.com>
2022-10-22 13:54:26 +0900

    Update sql.js

--- sql.js
+++ sql.js
@@ -1,7 +1,15 @@
-const pg = require('pg');
+// const pg = require('pg');
+const { Pool } = require("pg");
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
+// const pool = new pg.Pool({
+//   connectionString: process.env.DATABASE_URL
+// });
+const pool = new Pool({
+  user: "user",
+  host: "dashboard.render.com",
+  database: "fishbowling",
+  password: "zMFAUfVHzKChodCo06anA7oakRWwJmM3",
+  port: 5432,
 });
 
 module.exports = pool;
